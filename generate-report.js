23f03b9ba847caaaac730f7951656e5263eca89d
Wilson Lin <contact@wilsonl.in>
2017-01-02 23:19:12 +1300

    Added resource usages and changed tester to Siege

--- generate-report.js
+++ generate-report.js
@@ -1,5 +1,39 @@
 const fs = require('fs');
 
+let timeStarted;
+let times = {};
+(function() {
+    let lines = fs.readFileSync(__dirname + '/times.log', 'utf8').split(/[\r\n]+/).map(line => line.trim()).filter(line => !!line);
+    timeStarted = Number.parseInt(lines.splice(0, 1)[0]);
+    let currentTest;
+    lines.forEach(function(line) {
+        if (line[0] == '#') {
+            currentTest = line.slice(1);
+            times[currentTest] = {};
+        } else {
+            let parts = line.split(';');
+            times[currentTest][parts[0]] = {
+                started: Number.parseInt(parts[1]),
+                ended: Number.parseInt(parts[2]),
+            };
+        }
+    });
+})();
+
+let systemLoadData = [];
+(function() {
+    let lines = fs.readFileSync(__dirname + '/system-load.csv', 'utf8').split(/[\r\n]+/).map(line => line.trim()).filter(line => !!line);
+    lines.splice(0, 7);
+    lines.forEach(function(line, lineNo) {
+        let parts = line.split(',');
+        systemLoadData.push({
+            timestamp: timeStarted + (1000 * lineNo),
+            totalCpuUsage: Number.parseFloat(parts[0]),
+            memoryUsage: Number.parseFloat(parts[1]),
+        });
+    });
+})();
+
 let values = Object.create(null);
 let errors = Object.create(null);
 
@@ -12,10 +46,10 @@ fs.readdirSync(__dirname + '/results').forEach(testName => {
         let file = fs.readFileSync(`${__dirname}/results/${testName}/${testSubject}.log`, {encoding: 'utf8'});
         let regexpMatches;
 
-        regexpMatches = /^Requests per second:\s+([0-9.]+)/m.exec(file);
+        regexpMatches = /^Transaction rate:\s+([0-9.]+)/m.exec(file);
         let requestsPerSecond = (regexpMatches && regexpMatches[1]) | 0; // Will be zero if not found
 
-        regexpMatches = /^Non-2xx responses:\s+([0-9]+)/m.exec(file);
+        regexpMatches = /^Failed transactions:\s+([0-9]+)/m.exec(file);
         let totalErrors = (regexpMatches && regexpMatches[1]) | 0; // Will be zero if not found
 
         results[testSubject] = requestsPerSecond;
@@ -24,9 +58,9 @@ fs.readdirSync(__dirname + '/results').forEach(testName => {
 });
 
 let chartColors = JSON.stringify({
-    php: '#8892bf',
-    express: '#353535',
-    openresty: '#518451',
+    PHP: '#8892bf',
+    Express: '#353535',
+    OpenResty: '#518451',
 });
 
 let html = `
@@ -37,12 +71,31 @@ let html = `
 
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.css">
             <style>
-                #title, #background {
+                * {
                     box-sizing: border-box;
                 }
-                #chart-requests, #chart-errors {
+                #chart-requests, #chart-errors, .charts-group-test-sysload {
                     margin: 0 auto;
                     width: calc(100% - 40px);
+                }
+                main {
+                    background: rgb(230, 230, 230);
+                }
+                .charts-group-test-sysload-container {
+                    background: rgb(240, 240, 240);
+                }
+                .charts-group-test-sysload-container > h2 {
+                    background: rgba(0, 49, 100, 0.9);
+                    color: white;
+                    margin: 0;
+                    padding: 10px;
+                }
+                .charts-group-test-sysload {
+                    display: flex;
+                    width: 100%;
+                }
+                .chart-sysload-container {
+                    flex-grow: 1;
                 }
                 body {
                     margin: 0;
@@ -74,13 +127,27 @@ let html = `
 
             <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.js"></script>
+            <script>
+                var sysloadCharts = [];
+                function resizeCharts() {
+                    requestsChart.resize({
+                        height: document.documentElement.clientHeight - 80,
+                    });
+                    sysloadCharts.forEach(function(chart) {
+                        chart.resize({
+                            width: (document.documentElement.clientWidth - 40) / 2,
+                        })
+                    });
+                }
+            </script>
         </head>
 
-        <body onresize="resizeChart()" onorientationchange="resizeChart()">
+        <body onresize="resizeCharts()" onorientationchange="resizeCharts()">
             <h1 id="title">Load testing on </h1>
-            <div id="controls">
-                <button onclick="normaliseChart()">Normalise</button>
-            </div>`;
+            <main>
+                <div id="controls">
+                    <button onclick="normaliseChart()">Normalise</button>
+                </div>`;
 
 (function() {
     let chartColumns = [];
@@ -106,11 +173,6 @@ let html = `
     html += `
         <div id="chart-requests"></div>
         <script>
-            function resizeChart() {
-                requestsChart.resize({
-                    height: document.documentElement.clientHeight - 80,
-                });
-            }
             function normaliseChart() {
                 requestsChart.axis.max(30000);
             }
@@ -134,7 +196,7 @@ let html = `
                     }
                 },
             });
-            setTimeout(resizeChart, 1000);
+            setTimeout(resizeCharts, 1000);
         </script>
     `;
 })();
@@ -186,5 +248,85 @@ let html = `
         </script>
     `;
 })();
+
+html += '</main>';
+
+Object.keys(times).forEach(function(test) {
+    let timeData = times[test];
+    let graphTimeTicksCount = 0;
+    let cpuChartColumns = [];
+    let memoryChartColumns = [];
+    Object.keys(timeData).forEach(function(subject) {
+        let {started, ended} = timeData[subject];
+        
+        let systemLoadDataStart, systemLoadDataEnd;
+        for (let i = 0; i < systemLoadData.length; i++) {
+            let data = systemLoadData[i];
+            if (data.timestamp >= started) {
+                systemLoadDataStart = i - 1;
+                break;
+            }
+        }
+        for (let i = systemLoadData.length - 1; i >= 0; i--) {
+            let data = systemLoadData[i];
+            if (data.timestamp <= ended) {
+                systemLoadDataEnd = i + 1;
+                break;
+            }
+        }
+        let relevantSystemLoadData = systemLoadData.slice(systemLoadDataStart, systemLoadDataEnd + 1);
+
+        let ticksCount = systemLoadDataEnd - systemLoadDataStart + 1;
+        if (ticksCount > graphTimeTicksCount) {
+            graphTimeTicksCount = ticksCount;
+        }
+
+        cpuChartColumns.push([
+            subject,
+            ...Array(graphTimeTicksCount).fill(0).map((_, idx) => idx < relevantSystemLoadData.length ? relevantSystemLoadData[idx].totalCpuUsage : 0),
+        ]);
+
+        memoryChartColumns.push([
+            subject,
+            ...Array(graphTimeTicksCount).fill(0).map((_, idx) => idx < relevantSystemLoadData.length ? relevantSystemLoadData[idx].memoryUsage : 0),
+        ]);
+    });
+    [cpuChartColumns, memoryChartColumns] = [cpuChartColumns, memoryChartColumns].map(c => JSON.stringify(c));
+
+    html += `
+        <div class="charts-group-test-sysload-container">
+            <h2>${test}</h2>
+            <div class="charts-group-test-sysload">
+                <div class="chart-sysload-container">
+                    <h3>CPU</h3>
+                    <div class="chart-sysload-cpu" data-test="${test.toLowerCase()}"></div>
+                    <script>
+                        sysloadCharts[sysloadCharts.length] = c3.generate({
+                            bindto: '.chart-sysload-cpu[data-test="${test.toLowerCase()}"]',
+                            data: {
+                                columns: ${cpuChartColumns},
+                                colors: ${chartColors},
+                            }
+                        });
+                    </script>
+                </div>
+
+                <div class="chart-sysload-container">
+                    <h3>Memory</h3>
+                    <div class="chart-sysload-ram" data-test="${test.toLowerCase()}"></div>
+                    <script>
+                        sysloadCharts[sysloadCharts.length] = c3.generate({
+                            bindto: '.chart-sysload-ram[data-test="${test.toLowerCase()}"]',
+                            data: {
+                                columns: ${memoryChartColumns},
+                                colors: ${chartColors},
+                            }
+                        });
+                    </script>
+                </div>
+            </div>
+        </div>
+    `;
+});
 
 fs.writeFileSync(__dirname + '/report.html', html);
--- load-testing.sh
+++ load-testing.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
-# OVERRIDE: Don't quit on fail as some ab commands fail due to server
-# set -e
+set -e
 
 cd "$(dirname "$0")"
 
-mkdir -p results/hello-world results/json results/hmac results/db-get results/db-set
+TESTS[0]='hello-world'
+TESTS[1]='json'
+TESTS[2]='hmac'
+TESTS[3]='db-get'
+TESTS[4]='db-set'
+
+SUBJECTS[0]='PHP'
+SUBJECTS[1]='OpenResty'
+SUBJECTS[2]='Express'
+
+SUBJECT_URL_PATHS[0]='/load-testing/${TEST}.php'
+SUBJECT_URL_PATHS[1]=':1500/${TEST}'
+SUBJECT_URL_PATHS[2]=':3000/${TEST}'
+
+rm -rf results
+rm -f system-load.csv
+rm -f report.html
+rm -f times.log
+
+TIMESTAMP_STARTED=$(($(date +%s%N)/1000000))
+echo $TIMESTAMP_STARTED >> times.log
+dstat -cm --noheaders --float --output system-load.csv &>/dev/null &
+DSTAT_PID=$!
+
+echo "Started at $TIMESTAMP_STARTED"
+sleep 2 # Give some buffer room for beginning of system load data
 
 HOSTNAME="$1"
 if [[ -z "${HOSTNAME// }" ]]; then
@@ -13,68 +37,41 @@ if [[ -z "${HOSTNAME// }" ]]; then
 fi
 
 echo "Testing on $HOSTNAME..."
-echo
-echo "HELLO WORLD"
-echo
-printf "PHP..."
-ab -c10 -n100000 -r "http://$HOSTNAME/load-testing/hello-world.php" &> results/hello-world/php.log
-printf " done\n"
-printf "OpenResty..."
-ab -c10 -n100000 -r "http://$HOSTNAME:1500/hello-world" &> results/hello-world/openresty.log
-printf " done\n"
-printf "Express..."
-ab -c10 -n100000 -r "http://$HOSTNAME:3000/hello-world" &> results/hello-world/express.log
-printf " done\n"
-echo
-echo "JSON"
-echo
-printf "PHP..."
-ab -c10 -n100000 -r "http://$HOSTNAME/load-testing/json.php" &> results/json/php.log
-printf " done\n"
-printf "OpenResty..."
-ab -c10 -n100000 -r "http://$HOSTNAME:1500/json" &> results/json/openresty.log
-printf " done\n"
-printf "Express..."
-ab -c10 -n100000 -r "http://$HOSTNAME:3000/json" &> results/json/express.log
-printf " done\n"
-echo
-echo "HMAC"
-echo
-printf "PHP..."
-ab -c10 -n100000 -r "http://$HOSTNAME/load-testing/hmac.php" &> results/hmac/php.log
-printf " done\n"
-printf "OpenResty..."
-ab -c10 -n100000 -r "http://$HOSTNAME:1500/hmac" &> results/hmac/openresty.log
-printf " done\n"
-printf "Express..."
-ab -c10 -n100000 -r "http://$HOSTNAME:3000/hmac" &> results/hmac/express.log
-printf " done\n"
-echo
-echo "DB GET"
-echo
-printf "PHP..."
-ab -c10 -n10000 -r "http://$HOSTNAME/load-testing/db-get.php" &> results/db-get/php.log
-printf " done\n"
-printf "OpenResty..."
-ab -c10 -n10000 -r "http://$HOSTNAME:1500/db-get" &> results/db-get/openresty.log
-printf " done\n"
-printf "Express..."
-ab -c10 -n10000 -r "http://$HOSTNAME:3000/db-get" &> results/db-get/express.log
-printf " done\n"
-echo
-echo "DB SET"
-echo
-printf "PHP..."
-ab -c10 -n10000 -r "http://$HOSTNAME/load-testing/db-set.php" &> results/db-set/php.log
-printf " done\n"
-printf "OpenResty..."
-ab -c10 -n10000 -r "http://$HOSTNAME:1500/db-set" &> results/db-set/openresty.log
-printf " done\n"
-printf "Express..."
-ab -c10 -n10000 -r "http://$HOSTNAME:3000/db-set" &> results/db-set/express.log
-printf " done\n"
+echo "============================================================"
+
+for (( i=0; i<=$(( ${#TESTS[*]} -1 )); i++ ))
+do
+    export TEST="${TESTS[$i]}"
+    mkdir -p "results/$TEST"
+    echo "#$TEST" >> times.log
+
+    echo
+    echo "$TEST"
+    echo
+
+    for (( j=0; j<=$(( ${#SUBJECTS[*]} -1 )); j++ ))
+    do
+        export SUBJECT="${SUBJECTS[$j]}"
+        eval "URL=\"http://$HOSTNAME${SUBJECT_URL_PATHS[$j]}\""
+
+        printf "$SUBJECT..."
+
+        SUBJECT_TIMESTAMP_STARTED=$(($(date +%s%N)/1000000))
+        # ab -c1000 -n10000 -r "$URL" &> "results/$TEST/$SUBJECT.log"
+        siege -c100 -b -r100 -l/dev/null "$URL" &> "results/$TEST/$SUBJECT.log"
+        SUBJECT_TIMESTAMP_ENDED=$(($(date +%s%N)/1000000))
+        echo "$SUBJECT;$SUBJECT_TIMESTAMP_STARTED;$SUBJECT_TIMESTAMP_ENDED" >> times.log
+        
+        printf " done\n"
+
+        sleep 5
+    done
+done
+
+kill $DSTAT_PID
+wait $DSTAT_PID &>/dev/null
 
 node generate-report.js
-xdg-open ./report.html &
+xdg-open ./report.html &>/dev/null &
 
 exit 0
